296f1721c8028da0ddd8df28fa3feda6942e1197
0829v <48981839+0829v@users.noreply.github.com>
2026-05-25 10:54:20 +0000

    Learn Encapsulation

---
+++ MyConsoleApp/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿namespace MyConsoleApp;
+
+public class BankAccount
+{
+    private static int _counter = 0;
+
+    private string _accountNumber;
+    public string AccountNumber { get => _accountNumber; }
+
+    private string _ownerName;
+    public string OwnerName
+    {
+        get => _ownerName;
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                _ownerName = value;
+            }
+        }
+    }
+
+    private decimal _balance = 0;
+    public decimal Balance { get => _balance; }
+
+    public BankAccount(string ownerName)
+    {
+        _counter += 1;
+        string newAccountNumber = "ACC" + _counter.ToString().PadLeft(6, '0');
+        _accountNumber = newAccountNumber;
+        _ownerName = ownerName;
+        _balance = 0;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Console.WriteLine($"Your deposit request are {amount}.");
+        if (amount > 0)
+        {
+            _balance += amount;
+            Console.WriteLine($"You have been deposit {amount}, now your balance are {_balance}.");
+        }
+        else
+        {
+            Console.WriteLine("You only can deposit amount lager than 0.");
+        }
+    }
+
+    public bool Withdraw(decimal amount)
+    {
+        Console.WriteLine($"Your withdraw request are {amount}.");
+        if (amount > 0 && _balance >= amount)
+        {
+            _balance -= amount;
+            Console.WriteLine($"You have been withdraw {amount}, now your balance are {_balance}.");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Your balance amount are not enought to withdraw.");
+            return false;
+        }
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Your information: \nAccount number: {_accountNumber} \nOwner name: {_ownerName} \nBalance: {_balance}");
+    }
+}
---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using MyConsoleApp;
+
+BankAccount ac1 = new BankAccount("Nguyen Van A");
+ac1.Deposit(1500000);
+ac1.Withdraw(300000);
+ac1.Withdraw(2000000);
+BankAccount ac2 = new BankAccount("Nguyen Van Hoang");
+ac2.OwnerName = "";
+ac1.DisplayInfo();
+ac2.DisplayInfo();
